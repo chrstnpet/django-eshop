23e1b053431532c0eec5bba95d01806020c4f7ef
christina <chrstnpet@gmail.com>
2025-12-02 23:08:16 +0200

    Made changes to include bootstrap, custom styles and header logo + sandwich menu for responsiveness

--- lsshop/lsshop/settings.py
+++ lsshop/lsshop/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     'home',
     'crispy_forms',
-    'crispy_bootstrap4',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
@@ -128,4 +129,5 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
