1bf2ebde83f90298a79e1e6d525e1664e76ee4d6
christina <chrstnpet@gmail.com>
2025-12-17 17:30:58 +0200

    Made changes to home page views to bring top 4 items bought by users if they exist in the orders history

--- lsshop/home/views.py
+++ lsshop/home/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from orders.models import OrderProduct
+from products.models import Product
+from django.db.models import Sum
 
+# Bringing to the home page the 4 most bought items if there are more than 4 distinct items that have been bought, else I have my default ones that I want to promote
 def home(request):
-    return render(request, 'home/home.html', {'home':home})
+    popular_products_qs = (
+        OrderProduct.objects
+        .filter(ordered=True)
+        .values('product_variant__color_variant__product')
+        .annotate(total_sold=Sum('quantity'))
+        .order_by('-total_sold')[:4]
+    )
+
+    if popular_products_qs.count() >= 4:
+        product_ids = [
+            item['product_variant__color_variant__product']
+            for item in popular_products_qs
+        ]
+        popular_products = Product.objects.filter(id__in=product_ids)
+    else:
+        popular_products = None
+
+    return render(request, 'home/home.html', {'popular_products': popular_products})
